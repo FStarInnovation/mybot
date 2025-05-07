f4254656fc6f8887004b0cddde08d13ed84d5298
Евгений Звездин <zv@MacBook-Air-Evgenij.local>
2025-05-06 21:17:51 -0300

    Fix LlmBridgeController and enable vector search

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ResultsController;
 use App\Http\Controllers\Controller; // LLM Bridge
 use App\Http\Controllers\SupabaseController;
 use App\Http\Controllers\QueryController;
+use App\Http\Controllers\LlmBridgeController;
 
 Route::post('/scan-sites', [SiteScanController::class, 'scan']);
 Route::get('/results', [ResultsController::class, 'index']);
@@ -13,3 +14,4 @@ Route::post('/llm', [Controller::class, 'queryLLM']);
 Route::post('/supabase-test', [SupabaseController::class, 'testQuery']);
 Route::get('/supabase-test', [SupabaseController::class, 'testQuery']);
 Route::post('/query', [QueryController::class, 'handle']);
+Route::post('/query', [LlmBridgeController::class, 'query']);
