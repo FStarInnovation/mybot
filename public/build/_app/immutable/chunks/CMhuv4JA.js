98cc973a9b252cf54b7383d9ec94c1134b786829
FStarInnovation <teamzvezdin@gmail.com>
2025-06-25 18:52:54 -0300

    Собран фронтенд с новыми изменениями для цены за таблетку

---
+++ public/build/_app/immutable/chunks/CMhuv4JA.js
@@ -0,0 +1,1 @@
+import{s as e}from"./CVb1uaND.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
---
+++ public/build/_app/immutable/entry/start.o9MtnOwj.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/CVb1uaND.js";export{o as load_css,r as start};
