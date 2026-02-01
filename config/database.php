1b8763b8ec6c6bbe5f11f83fda37d76474f4a838
FStarInnovation <teamzvezdin@gmail.com>
2026-02-01 16:57:08 -0300

    fix: disable Horizon auto-discovery in composer.json

--- config/database.php
+++ config/database.php
@@ -72,6 +72,14 @@ return [
             'retry_interval' => 100,
         ],
 
+        'horizon' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DB', '0'),
+        ],
+
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST'),
