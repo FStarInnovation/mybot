2085ac505939e19e67251110eea069b4d468458e
FStarInnovation <teamzvezdin@gmail.com>
2026-02-01 16:45:03 -0300

    fix: disable HorizonServiceProvider to fix Redis config error

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\HorizonServiceProvider::class,
+    // App\Providers\HorizonServiceProvider::class, // Временно отключено - проблема с Redis
     App\Providers\TelescopeServiceProvider::class,
 ];
